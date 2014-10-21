3b55af4fe87faaf81c822ec64adb89dc1abe608f
Aug <heyaugust@gmail.com>
2014-10-20 23:36:05 -0400

    added user login

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
+
+# user 1 : user1@gmail.com, password
